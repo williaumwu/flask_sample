268c3da91fda0e936aa3e6f0d88e503ef9833ae0
Gary <gear@thytruth.com>
2015-12-29 16:02:44 -0800

    changed name from .env to app.env

--- config.py
+++ config.py
@@ -5,13 +5,12 @@ import os
 class BaseConfig(object):
     SECRET_KEY = os.environ['SECRET_KEY']
     DEBUG = os.environ['DEBUG']
-    DB_NAME = os.environ['DB_NAME']
-    DB_USER = os.environ['DB_USER']
-    DB_PASS = os.environ['DB_PASS']
-    DB_SERVICE = os.environ['DB_SERVICE']
-    DB_PORT = os.environ['DB_PORT']
+    MYSQL_DB = os.environ['MYSQL_DB']
+    MYSQL_USER = os.environ['MYSQL_USER']
+    MYSQL_PASS = os.environ['MYSQL_PASS']
+    MYSQL_HOST = os.environ['MYSQL_HOST']
+    MYSQL_PORT = os.environ['MYSQL_PORT']
     SQLALCHEMY_DATABASE_URI = 'mysql://{0}:{1}@{2}:{3}/{4}'.format(
-        DB_USER, DB_PASS, DB_SERVICE, DB_PORT, DB_NAME
+        MYSQL_USER, MYSQL_PASS, MYSQL_HOST, MYSQL_PORT, MYSQL_DB
     )
-
 
