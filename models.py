80e07eeda10fea292cf28c0114a2c9c8927f91ea
Gary <gear@thytruth.com>
2016-01-06 15:44:30 -0800

    updated pkges and db schema

--- models.py
+++ models.py
@@ -8,9 +8,10 @@ class Post(db.Model):
     __tablename__ = 'posts'
 
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String, nullable=False)
+    text = db.Column(db.String(16), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False)
 
     def __init__(self, text):
         self.text = text
         self.date_posted = datetime.datetime.now()
+
