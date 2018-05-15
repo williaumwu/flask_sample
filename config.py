2f2d0e9aa0087b1eb25f19d82b0c864a3c8f1491
Automatic Script <you@example.com>
2018-05-15 05:16:20 +0000

    update variables

--- config.py
+++ config.py
@@ -5,12 +5,11 @@ import os
 class BaseConfig(object):
     SECRET_KEY = os.environ['SECRET_KEY']
     DEBUG = os.environ['DEBUG']
-    DB_NAME = os.environ['DB_NAME']
-    DB_USER = os.environ['DB_USER']
-    DB_PASS = os.environ['DB_PASS']
-    DB_HOST = os.environ['DB_HOST']
-    DB_PORT = os.environ['DB_PORT']
+    DB_NAME = os.environ['MYSQL_DB_NAME']
+    DB_USER = os.environ['MYSQL_DB_USER']
+    DB_PASS = os.environ['MYSQL_DB_PASSWORD']
+    DB_HOST = os.environ['MYSQL_HOST']
+    DB_PORT = os.environ['MYSQL_PORT']
     SQLALCHEMY_DATABASE_URI = 'mysql://{0}:{1}@{2}:{3}/{4}'.format(
         DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
     )
-
