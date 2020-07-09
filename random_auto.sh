5a8bf02cc35cfebc5a2501a4de63012b8b6683d4
Gary <gary@elasticdev.io>
2020-07-09 22:17:53 +0800

    update with ci branch

---
+++ random_auto.sh
@@ -0,0 +1,47 @@
+cp -rp test_fail.py test.py
+./auto_change.sh
+sleep 1800
+
+cp -rp test_succeed.py test.py
+./auto_change.sh
+sleep 1800
+
+cp -rp test_succeed.py test.py
+./auto_change.sh
+sleep 1800
+
+cp -rp test_succeed.py test.py
+./auto_change.sh
+sleep 1800
+
+cp -rp test_fail.py test.py
+./auto_change.sh
+sleep 1800
+
+cp -rp test_fail.py test.py
+./auto_change.sh
+sleep 1800
+
+cp -rp test_succeed.py test.py
+./auto_change.sh
+sleep 1800
+
+cp -rp test_succeed.py test.py
+./auto_change.sh
+sleep 1800
+
+cp -rp test_succeed.py test.py
+./auto_change.sh
+sleep 1800
+
+cp -rp test_succeed.py test.py
+./auto_change.sh
+sleep 1800
+
+cp -rp test_fail.py test.py
+./auto_change.sh
+sleep 1800
+
+cp -rp test_succeed.py test.py
+./auto_change.sh
+
