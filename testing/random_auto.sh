33d6186a3e3aa87631f7503263da70a3f302046e
Gary <gary@elasticdev.io>
2022-01-11 21:17:17 +0800

    'update testing outlines'

---
+++ testing/random_auto.sh
@@ -0,0 +1,47 @@
+cp -rp testing/test_fail.py testing/test.py
+./testing/auto_change.sh
+sleep 1800
+
+cp -rp testing/test_succeed.py testing/test.py
+./testing/auto_change.sh
+sleep 1800
+
+cp -rp testing/test_succeed.py testing/test.py
+./testing/auto_change.sh
+sleep 1800
+
+cp -rp testing/test_succeed.py testing/test.py
+./testing/auto_change.sh
+sleep 1800
+
+cp -rp testing/test_fail.py testing/test.py
+./testing/auto_change.sh
+sleep 1800
+
+cp -rp testing/test_fail.py testing/test.py
+./testing/auto_change.sh
+sleep 1800
+
+cp -rp testing/test_succeed.py testing/test.py
+./testing/auto_change.sh
+sleep 1800
+
+cp -rp testing/test_succeed.py testing/test.py
+./testing/auto_change.sh
+sleep 1800
+
+cp -rp testing/test_succeed.py testing/test.py
+./testing/auto_change.sh
+sleep 1800
+
+cp -rp testing/test_succeed.py testing/test.py
+./testing/auto_change.sh
+sleep 1800
+
+cp -rp testing/test_fail.py testing/test.py
+./testing/auto_change.sh
+sleep 1800
+
+cp -rp testing/test_succeed.py testing/test.py
+./testing/auto_change.sh
+
